75eb21bae7f96de2458dc8cd2288b6fca93db552
= <=>
2020-01-31 09:38:35 +0200

    build process

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,8 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+    elixir(function(mix){
+    	mix.sass('main.scss')
+    	.coffee('module.coffee');
+    });
